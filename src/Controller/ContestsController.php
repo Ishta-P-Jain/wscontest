94c7d8eccf4857274c526f36982b5957581d2088
Sam Wilson <sam@samwilson.id.au>
2026-08-03 13:58:26 +0800

    Update `wikisource/api` and other dependencies

--- src/Controller/ContestsController.php
+++ src/Controller/ContestsController.php
@@ -81,7 +81,7 @@ class ContestsController extends AbstractController {
 		$username = $this->getLoggedInUsername( $session );
 
 		// If a user ID is requested, show only the scores for that user.
-		$userId = $request->get( 'u' );
+		$userId = $request->query->get( 'u' );
 		if ( $userId ) {
 			return $this->render( 'contests_viewuser.html.twig', [
 				'contest' => $contest,
